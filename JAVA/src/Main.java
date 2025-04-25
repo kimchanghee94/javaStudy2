7cfde687d820bea190071988336ddc47cee389fc
kimchanghee <kimchanghee94@naver.com>
2025-04-25 13:11:02 +0900

    #main 1. java파일 gitignore처리

--- JAVA/src/Main.java
+++ JAVA/src/Main.java
@@ -1,5 +1,12 @@
+import 완전범죄.FullCrime;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FullCrime fc = new FullCrime();
+
+        int[][] input = new int[][]{{1, 2}, {2, 3}};
+
+        System.out.println(fc.solution(input,4,4));
     }
 }
---
+++ JAVA/src/완전범죄/FullCrime.java
@@ -0,0 +1,11 @@
+package 완전범죄;
+
+public class FullCrime {
+    public int solution(int[][] info, int n, int m) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+}
