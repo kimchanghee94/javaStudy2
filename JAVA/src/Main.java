c7e5b21bea95adddec4b12bf1c6ffe27d8a2d23c
kimchanghee <kimchanghee94@naver.com>
2025-05-09 12:57:56 +0900

    #main 1. 온라인테스트 완료

--- JAVA/src/Main.java
+++ JAVA/src/Main.java
@@ -1,13 +1,10 @@
-import 지폐접기.Solution;
+import 지게차와크레인.Solution;
 
 public class Main {
 
     public static void main(String[] args) {
         Solution sol = new Solution();
 
-        int[] wallet = {30, 15};
-        int[] bill = {26, 17};
-
-        System.out.println(sol.solution(wallet, bill));
+        System.out.println(sol.solution());
     }
 }
