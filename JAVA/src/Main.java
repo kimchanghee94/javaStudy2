a065e01ae89b2bae946b6b485aafd2dba78af680
kimchanghee <kimchanghee94@naver.com>
2025-05-23 08:17:05 +0900

    #main 1. mergeSortedArray

--- JAVA/src/Main.java
+++ JAVA/src/Main.java
@@ -1,10 +1,11 @@
-import 지게차와크레인.Solution;
+import MergeSortedArray.Solution;
 
 public class Main {
 
     public static void main(String[] args) {
         Solution sol = new Solution();
 
-        System.out.println(sol.solution());
+        int[] nums1 = {1,2,3,0,0,0};
+        sol.merge(nums1, 3, new int[]{2,5,6}, 3);
     }
 }
---
+++ JAVA/src/MergeSortedArray/Solution.java
@@ -0,0 +1,25 @@
+package MergeSortedArray;
+
+import java.util.Arrays;
+
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int idx = nums1.length - 1;
+
+        while(m>0 && n>0){
+            if(nums1[m-1] >= nums2[n-1]){
+                nums1[idx--] = nums1[m-1];
+                m--;
+            }else{
+                nums1[idx--] = nums2[n-1];
+                n--;
+            }
+        }
+
+        if(n>0){
+            for(int i=0; i<n; i++){
+                nums1[i] = nums2[i];
+            }
+        }
+    }
+}
