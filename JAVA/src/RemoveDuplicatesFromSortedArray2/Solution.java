4013a0665f04f8916a31e3dd598bac728c79090b
kimchanghee <kimchanghee94@naver.com>
2025-05-23 10:10:45 +0900

    #main 1. removeDuplicatesFromSortedArray2

---
+++ JAVA/src/RemoveDuplicatesFromSortedArray2/Solution.java
@@ -0,0 +1,19 @@
+package RemoveDuplicatesFromSortedArray2;
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k=0;
+
+        for(int i=1, dup=0; i<nums.length; i++){
+            if(dup == 0 && nums[k] == nums[i]){
+                dup++;
+                nums[++k] = nums[i];
+            }else if(nums[k] != nums[i]){
+                dup = 0;
+                nums[++k] = nums[i];
+            }
+        }
+
+        return k+1;
+    }
+}
