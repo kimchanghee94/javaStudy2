02e280ef9cf808451d0b73d3354a308de41f42f3
kimchanghee <kimchanghee94@naver.com>
2025-05-29 12:42:38 +0900

    #main 1. H-Index풀이 수정

--- JAVA/src/HIndex/Solution.java
+++ JAVA/src/HIndex/Solution.java
@@ -4,15 +4,15 @@ import java.util.*;
 
 public class Solution {
     public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
         int ans = 0;
 
-        Integer[] arrList = Arrays.stream(citations).boxed().toArray(Integer[]::new);
-
-        Arrays.sort(arrList, Collections.reverseOrder());
-
-        for(int i=0; i<arrList.length; i++){
-            if(i+1 > arrList[i]){
-                ans = i+1;
+        for(int i=0; i<n; i++){
+            int h = n-i;
+            if(citations[i] >= h){
+                ans = h;
+                break;
             }
         }
 
