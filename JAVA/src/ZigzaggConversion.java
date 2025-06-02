4477e39fce16c36aa70bab1c4f9a0c181db12f07
kimchanghee <kimchanghee94@naver.com>
2025-06-02 10:26:49 +0900

    #main 1. ZigzaggConversion => 구현

---
+++ JAVA/src/ZigzaggConversion.java
@@ -0,0 +1,30 @@
+public class ZigzaggConversion {
+    public String convert(String s, int numRows) {
+        StringBuilder ans = new StringBuilder();
+
+        if(numRows == 1 || s.length() == 1 || s.length() <= numRows){
+            return s;
+        }
+
+        for(int i=0; i<numRows; i++){
+            ans.append(s.charAt(i));
+
+            int j=i;
+
+            while(true){
+                int tmp = (2*numRows - 2*(i+1));
+
+                if(j + tmp >= s.length()) break;
+                if(tmp != 0) ans.append(s.charAt(j + tmp));
+                j += tmp;
+
+                tmp = (2*numRows - 2*(numRows - i));
+                if(j + tmp >= s.length()) break;
+                if(tmp != 0) ans.append(s.charAt(j + tmp));
+                j += tmp;
+            }
+        }
+
+        return ans.toString();
+    }
+}
