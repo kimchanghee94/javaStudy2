fe8e5a8ffbd64cf539b121c7c77f217b27bbe083
kimchanghee <kimchanghee94@naver.com>
2025-06-06 00:13:45 +0900

    #main 1. ValidPalindrome 시간복잡도 단축

--- JAVA/src/ValidPalindrome/Solution.java
+++ JAVA/src/ValidPalindrome/Solution.java
@@ -3,20 +3,21 @@ package ValidPalindrome;
 public class Solution {
     public boolean isPalindrome(String s) {
         boolean ans = true;
-        s = s.toLowerCase();
-        StringBuilder sb = new StringBuilder();
+        char[] filter = new char[s.length()];
 
+        int cnt = 0;
         for(int i=0; i<s.length(); i++){
             char tmp = s.charAt(i);
 
             if('a' <= tmp && tmp <= 'z' || '0' <= tmp && tmp <= '9'){
-                sb.append(tmp);
+                filter[cnt++] = tmp;
+            }else if('A' <= tmp && tmp <= 'Z'){
+                filter[cnt++]  = (char)(tmp + 32);
             }
         }
 
-        int len = sb.length();
-        for(int i=0; i<len/2; i++){
-            if(sb.charAt(i) != sb.charAt(len-i-1)){
+        for(int i=0; i<cnt/2; i++){
+            if(filter[i] != filter[cnt-i-1]){
                 ans = false;
                 break;
             }
