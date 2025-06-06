36692d11423be62880b9062ffc480849d55794a8
kimchanghee <kimchanghee94@naver.com>
2025-06-06 15:46:35 +0900

    #main 1. IsSubsequence => 투포인터

---
+++ JAVA/src/IsSubsequence/Solution.java
@@ -0,0 +1,18 @@
+package IsSubsequence;
+
+public class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int idx = 0;
+
+        if(s.length() == 0) return true;
+
+        for(int i=0; i<t.length(); i++){
+            if(s.charAt(idx) == t.charAt(i)){
+                idx++;
+                if(idx == s.length()) return true;
+            }
+        }
+
+        return false;
+    }
+}
