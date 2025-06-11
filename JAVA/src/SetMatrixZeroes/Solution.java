8ae6caafd790b532c7128742fc3ccdfa50aa4886
kimchanghee <kimchanghee94@naver.com>
2025-06-11 18:27:35 +0900

    #main 1. SetMatrixZeroes => Matrix알고리즘에서 대부분 in-place알고리즘으로 풀자(새로 배열추가없이 처리하는것)

---
+++ JAVA/src/SetMatrixZeroes/Solution.java
@@ -0,0 +1,19 @@
+package SetMatrixZeroes;
+
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        int M=matrix.length, N=matrix[0].length;
+        boolean firR=false, firC=false;
+
+        for(int i=0; i<N; i++) if(matrix[0][i] == 0) firR = true;
+        for(int i=0; i<M; i++) if(matrix[i][0] == 0) firC = true;
+        for(int i=0; i<M; i++) for(int j=0; j<N; j++)
+            if(matrix[i][j] == 0){
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        for(int i=1; i<M; i++) for(int j=1; j<N; j++) if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j]=0;
+        if(firR == true) for(int i=0; i<N; i++) matrix[0][i] = 0;
+        if(firC == true) for(int i=0; i<M; i++) matrix[i][0] = 0;
+    }
+}
