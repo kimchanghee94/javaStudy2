89acd369ffe6044d58d9ee5687ddcb4fdd5ac470
kimchanghee <kimchanghee94@naver.com>
2025-06-12 18:04:50 +0900

    #main 1. ValidAnagram => 배열 hash 처리, 두번째는 유니코드에 대한 처리

---
+++ JAVA/src/ValidAnagram/Solution.java
@@ -0,0 +1,25 @@
+package ValidAnagram;
+
+import java.util.*;
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        /*if(s.length() != t.length()) return false;
+
+        int[] hash = new int[26];
+        for(char c : s.toCharArray()) hash[c-'a']++;
+        for(char c : t.toCharArray()) if(--hash[c-'a'] < 0) return false;
+        return true;*/
+
+        int[] sa = s.codePoints().toArray();
+        int[] ta = t.codePoints().toArray();
+        if(sa.length != ta.length) return false;
+        Map<Integer, Integer> hash = new HashMap<>();
+        for(int c : sa) hash.put(c, hash.getOrDefault(c,0)+1);
+        for(int c : ta) {
+            hash.put(c, hash.getOrDefault(c,0)-1);
+            if(hash.get(c)<0) return false;
+        }
+        return true;
+    }
+}
