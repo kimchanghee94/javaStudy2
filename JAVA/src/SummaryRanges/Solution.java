99995dc10b73fa06c8b8ed21ab50bc77cb242c16
kimchanghee <kimchanghee94@naver.com>
2025-06-16 18:11:20 +0900

    #main 1. SummaryRanges => intervals, N값 길이보다 한칸더돌게하면서 마지막값 처리하기

--- JAVA/src/SummaryRanges/Solution.java
+++ JAVA/src/SummaryRanges/Solution.java
@@ -5,19 +5,17 @@ import java.util.*;
 public class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> ans = new ArrayList<>();
-        int currentIndex = 0;
-        for (int i=0;i < nums.length;i++) {
-            if((i+1 < nums.length) && (nums[i] + 1 == nums[i+1])) {
-                continue;
+        int N=nums.length;
+        int strt=0;
+
+        for(int i=1; i<=N; i++){
+            if(i==N||nums[i-1]+1!=nums[i]){
+                if(nums[strt]==nums[i-1]) ans.add(nums[strt]+"");
+                else ans.add(nums[strt]+"->"+nums[i-1]);
+                strt=i;
             }
-            if(currentIndex == i) {
-                ans.add(String.valueOf(nums[currentIndex]));
-                currentIndex = currentIndex + 1;
-                continue;
-            }
-            ans.add(String.valueOf(nums[currentIndex]) + "->" + String.valueOf(nums[i]));
-            currentIndex = i+1;
         }
+
         return ans;
     }
 }
