e7a35e0be9a685f86a7f0e1c5d3a08a3b8b5b738
kimchanghee <kimchanghee94@naver.com>
2025-06-20 17:59:14 +0900

    #main 1. InsertInterval => 최소 코드로 풀이

--- JAVA/src/InsertInterval/Solution.java
+++ JAVA/src/InsertInterval/Solution.java
@@ -5,24 +5,15 @@ import java.util.*;
 public class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> ans = new ArrayList<>();
-        int ns = newInterval[0], ne = newInterval[1];
-        boolean flag = false;
-        if(intervals.length == 0) return new int[][]{newInterval};
-        for(int i=0; i<=intervals.length; i++){
-            int[] interval;
-            if(i==intervals.length){
-                if(flag == true) break;
-                interval = newInterval;
-            }
-            else if(newInterval[0] <= intervals[i][0] && flag==false){
-                interval = newInterval;
-                flag = true;
-                i--;
-            }
-            else interval = intervals[i];
-            if(ans.size() == 0 || ans.get(ans.size()-1)[1] < interval[0]) ans.add(interval);
-            else if(ans.get(ans.size()-1)[1] >= interval[0]) ans.get(ans.size()-1)[1] = Math.max(ans.get(ans.size()-1)[1], interval[1]);
-        }
+
+        int i=0, N=intervals.length;
+
+        while(i<N && intervals[i][1] < newInterval[0]) ans.add(intervals[i++]);
+        while(i<N && intervals[i][0] <= newInterval[1])
+            newInterval = new int[]{Math.min(newInterval[0], intervals[i][0]), Math.max(newInterval[1], intervals[i++][1])};
+        ans.add(newInterval);
+        while(i<N) ans.add(intervals[i++]);
+
         return ans.toArray(new int[ans.size()][]);
     }
 }
