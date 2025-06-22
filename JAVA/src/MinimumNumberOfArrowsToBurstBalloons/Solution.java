8f0eb24ebfa3c9b6f2c983d308d7696e68463f57
kimchanghee <kimchanghee94@naver.com>
2025-06-22 14:33:39 +0900

    #main 1. MinimumNumberOfArrowsToBurstBallons => 2차 풀이 (시간복잡도 고려)

--- JAVA/src/MinimumNumberOfArrowsToBurstBalloons/Solution.java
+++ JAVA/src/MinimumNumberOfArrowsToBurstBalloons/Solution.java
@@ -4,12 +4,14 @@ import java.util.*;
 
 public class Solution {
     public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points, (a,b) -> Integer.compare(a[0], b[0]));
-        List<int[]> ans = new ArrayList<>();
+        Arrays.sort(points,(a,b)->Integer.compare(a[1],b[1]));
+        int ans=1,e=points[0][1];
         for(int[] point:points){
-            if(ans.size()==0||ans.get(ans.size()-1)[1] < point[0]) ans.add(point);
-            else ans.get(ans.size()-1)[1]=Math.min(point[1], ans.get(ans.size()-1)[1]);
+            if(e<point[0]){
+                ans++;
+                e=point[1];
+            }
         }
-        return ans.size();
+        return ans;
     }
 }
