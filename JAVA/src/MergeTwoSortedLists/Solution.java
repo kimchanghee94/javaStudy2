5bfd6f844d2774ce87f596eda4175ff09db9d73b
kimchanghee <kimchanghee94@naver.com>
2025-06-23 18:09:28 +0900

    #main 1. MergeTwoSortedLists => LinkedList

---
+++ JAVA/src/MergeTwoSortedLists/Solution.java
@@ -0,0 +1,38 @@
+package MergeTwoSortedLists;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+
+        while(list1 != null || list2 != null){
+            if(list1 != null && list2 != null){
+                if(list1.val <= list2.val){
+                    cur.next = new ListNode(list1.val);
+                    list1 = list1.next;
+                }else{
+                    cur.next = new ListNode(list2.val);
+                    list2 = list2.next;
+                }
+            }
+            else if(list1 != null) {
+                cur.next = new ListNode(list1.val);
+                list1 = list1.next;
+            }
+            else if(list2 != null) {
+                cur.next = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+            cur=cur.next;
+        }
+        return dummy.next;
+    }
+}
