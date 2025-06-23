ee0ba9790ebb1e7455f5444ca1ed3fcf58ce989c
kimchanghee <kimchanghee94@naver.com>
2025-06-23 16:48:08 +0900

    #main 1. BasicCalculator => 스택기반 알고리즘

---
+++ JAVA/src/BasicCalculator/Solution.java
@@ -0,0 +1,36 @@
+package BasicCalculator;
+
+import java.util.*;
+
+public class Solution {
+    public int calculate(String s) {
+        int ans=0, num=0, sign=1;
+        Stack<Integer> stack = new Stack<>();
+
+        for(char c:s.toCharArray()){
+            if(c=='+'){
+                ans+=sign*num;
+                num=0;
+                sign=1;
+            }else if(c=='-'){
+                ans+=sign*num;
+                num=0;
+                sign=-1;
+            }else if(c=='('){
+                stack.push(ans);
+                stack.push(sign);
+                ans=0;
+                sign=1;
+            }else if(c==')'){
+                ans+=sign*num;
+                num=0;
+                ans*=stack.pop();
+                ans+=stack.pop();
+            }else if(c!=' '){
+                num = num*10 + (c-'0');
+            }
+        }
+
+        return ans + (sign*num);
+    }
+}
