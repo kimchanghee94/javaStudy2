4c667f706366d93dd82f20d8bf5164fd121a5883
kimchanghee <kimchanghee94@naver.com>
2025-06-23 18:12:47 +0900

    #main 1. MergeTwoSortedLists => 코드 간결 처리

--- JAVA/src/MergeTwoSortedLists/Solution.java
+++ JAVA/src/MergeTwoSortedLists/Solution.java
@@ -13,26 +13,17 @@ public class Solution {
         ListNode dummy = new ListNode();
         ListNode cur = dummy;
 
-        while(list1 != null || list2 != null){
-            if(list1 != null && list2 != null){
-                if(list1.val <= list2.val){
-                    cur.next = new ListNode(list1.val);
-                    list1 = list1.next;
-                }else{
-                    cur.next = new ListNode(list2.val);
-                    list2 = list2.next;
-                }
-            }
-            else if(list1 != null) {
-                cur.next = new ListNode(list1.val);
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                cur.next = list1;
                 list1 = list1.next;
-            }
-            else if(list2 != null) {
-                cur.next = new ListNode(list2.val);
+            }else{
+                cur.next = list2;
                 list2 = list2.next;
             }
             cur=cur.next;
         }
+        cur.next = list1 != null ? list1 : list2;
         return dummy.next;
     }
 }
