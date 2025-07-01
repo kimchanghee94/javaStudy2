8c5a8d5b0989aeea2b4305d4889e9ce0dc0e9b63
kimchanghee <kimchanghee94@naver.com>
2025-07-01 13:04:05 +0900

    #main 1. MaximumDepthOfBinaryTree => BinaryTree + BFS or DFS

---
+++ JAVA/src/MaximumDepthOfBinaryTree/Solution.java
@@ -0,0 +1,44 @@
+package MaximumDepthOfBinaryTree;
+
+import java.util.*;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+
+        //DFS
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+
+        //BFS
+        /*
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int ans=0;
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i=0; i<size; i++){
+                TreeNode tn = queue.poll();
+                if(tn.left != null) queue.offer(tn.left);
+                if(tn.right != null) queue.offer(tn.right);
+            }
+            ans++;
+        }
+
+        return ans;
+        */
+    }
+}
