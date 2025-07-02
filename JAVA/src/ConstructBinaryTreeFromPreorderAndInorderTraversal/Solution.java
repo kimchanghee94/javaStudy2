635a640cd5b6d28f0fd33151cea154721ffbe7cd
kimchanghee <kimchanghee94@naver.com>
2025-07-02 13:31:03 +0900

    #main 1. ConstructBinaryTreeFromPreorderAndInorderTraversal => BinaryTree + inorder,preorder

---
+++ JAVA/src/ConstructBinaryTreeFromPreorderAndInorderTraversal/Solution.java
@@ -0,0 +1,39 @@
+package ConstructBinaryTreeFromPreorderAndInorderTraversal;
+
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    Map<Integer, Integer> map = new HashMap<>();
+    int preIdx = 0;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for(int i=0; i<inorder.length; i++) map.put(inorder[i], i);
+        return dfs(preorder, 0, inorder.length-1);
+    }
+
+    private TreeNode dfs(int[] pre, int left, int right){
+        if(left > right) return null;
+
+        int rootVal = pre[preIdx++];
+        TreeNode root = new TreeNode(rootVal);
+        int rootIdx = map.get(rootVal);
+
+        root.left = dfs(pre, left, rootIdx-1);
+        root.right = dfs(pre, rootIdx+1, right);
+
+        return root;
+    }
+}
