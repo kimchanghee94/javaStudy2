77601139e03904744f9c980dff4db319664b6124
kimchanghee <kimchanghee94@naver.com>
2025-07-03 15:13:40 +0900

    #main 1. FlatternBinaryTreeToLinkedList => BinaryTree + 참조주소

---
+++ JAVA/src/FlatternBinaryTreeToLinkedList/Solution.java
@@ -0,0 +1,31 @@
+package FlatternBinaryTreeToLinkedList;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public void flatten(TreeNode root) {
+        TreeNode cur = root;
+
+        while(cur != null){
+            if(cur.left != null){
+                TreeNode rMost = cur.left;
+                while(rMost.right != null) rMost = rMost.right;
+                rMost.right = cur.right;
+                cur.right = cur.left;
+                cur.left = null;
+            }
+            cur = cur.right;
+        }
+    }
+}
