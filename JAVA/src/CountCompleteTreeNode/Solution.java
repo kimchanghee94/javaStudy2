48f7e2d6f524256cf2820a36510dbe2515a32127
kimchanghee <kimchanghee94@naver.com>
2025-07-07 13:02:12 +0900

    #main 1. CountCompleteTreeNode => BinaryTree

---
+++ JAVA/src/CountCompleteTreeNode/Solution.java
@@ -0,0 +1,32 @@
+package CountCompleteTreeNode;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public int countNodes(TreeNode root) {
+        if(root == null) return 0;
+        int lH = getH(root.left);
+        int rH = getH(root.right);
+        if(lH == rH) return (1 << lH) + countNodes(root.right);
+        else return (1 << rH) + countNodes(root.left);
+    }
+    private int getH(TreeNode root){
+        int height = 0;
+        while(root != null){
+            height++;
+            root = root.left;
+        }
+        return height;
+    }
+}
