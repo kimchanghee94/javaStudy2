73923583d9dcf6be902efb35c3eccbd3550ab92f
kimchanghee <kimchanghee94@naver.com>
2025-07-08 12:11:12 +0900

    #main 1. AverageOfLevelsInBinaryTree => BinaryTree + BFS

---
+++ JAVA/src/AverageOfLevelsInBinaryTree/Solution.java
@@ -0,0 +1,39 @@
+package AverageOfLevelsInBinaryTree;
+
+import java.util.*;
+
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+}
+
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            double val = 0;
+            for(int i=0; i<size; i++){
+                TreeNode node = q.poll();
+                val += node.val;
+
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
+            }
+            ans.add(val/size);
+        }
+
+        return ans;
+    }
+}
