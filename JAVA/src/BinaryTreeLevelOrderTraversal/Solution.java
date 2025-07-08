c571a35f14d86fb7643d62f9c05cfb32bf1de4ae
kimchanghee <kimchanghee94@naver.com>
2025-07-08 12:17:09 +0900

    #main 1. BinaryTreeLevelOrderTraversal => BinaryTree + BFS

---
+++ JAVA/src/BinaryTreeLevelOrderTraversal/Solution.java
@@ -0,0 +1,38 @@
+package BinaryTreeLevelOrderTraversal;
+
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> tmp = new ArrayList<>();
+            for(int i=0; i<size; i++){
+                TreeNode node = q.poll();
+                tmp.add(node.val);
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
+            }
+            ans.add(tmp);
+        }
+        return ans;
+    }
+}
