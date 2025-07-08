8810be92754be18025944a443946bcb5cbb83453
kimchanghee <kimchanghee94@naver.com>
2025-07-08 14:39:56 +0900

    #main 1. MinimumAbsoluteDifferenceInBST => BST + DFS

---
+++ JAVA/src/MinimumAbsoluteDifferenceInBST/Solution.java
@@ -0,0 +1,33 @@
+package MinimumAbsoluteDifferenceInBST;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    int ans = Integer.MAX_VALUE;
+    TreeNode prev = null;
+
+    public int getMinimumDifference(TreeNode root) {
+        inorder(root);
+        return ans;
+    }
+
+    private void inorder(TreeNode root){
+        if(root==null) return;
+
+        inorder(root.left);
+        if(prev != null) ans = Math.min(Math.abs(prev.val-root.val), ans);
+        prev = root;
+        inorder(root.right);
+    }
+}
