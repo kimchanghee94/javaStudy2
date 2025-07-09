75aff63f30f7c8c168199ac9e2903bd1b2150c35
kimchanghee <kimchanghee94@naver.com>
2025-07-09 15:37:42 +0900

    #main 1. CloneGraph => Graph + DFS

---
+++ JAVA/src/CloneGraph/Solution.java
@@ -0,0 +1,37 @@
+package CloneGraph;
+
+import java.util.*;
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+
+public class Solution {
+    Node[] flag = new Node[102];
+
+    public Node cloneGraph(Node node) {
+        if(node == null) return null;
+        return dfs(node);
+    }
+
+    private Node dfs(Node node){
+        if(flag[node.val] != null) return flag[node.val];
+        Node copy = new Node(node.val);
+        flag[copy.val] = copy;
+        for(Node neighbor : node.neighbors) copy.neighbors.add(dfs(neighbor));
+        return copy;
+    }
+}
