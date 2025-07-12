c4d6d86acf93abc2713f190e64b17f22e6ae0285
kimchanghee <kimchanghee94@naver.com>
2025-07-12 10:52:15 +0900

    #main 1. ConvertSortedArrayToBinarySearchTree => DivideAndConquer + DFS

---
+++ JAVA/src/ConvertSortedArrayToBinarySearchTree/Solution.java
@@ -0,0 +1,28 @@
+package ConvertSortedArrayToBinarySearchTree;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return dfs(nums, 0, nums.length-1);
+    }
+
+    private TreeNode dfs(int[] nums, int left, int right){
+        if(left > right) return null;
+        int mid = (left + right)/2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = dfs(nums, left, mid-1);
+        node.right = dfs(nums, mid+1, right);
+        return node;
+    }
+}
