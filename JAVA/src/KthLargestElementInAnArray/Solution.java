120b9389a4aaa3c61f14b7f95813fbfe09d2fa17
kimchanghee <kimchanghee94@naver.com>
2025-07-18 11:19:41 +0900

    #main 1. KthLargestElementInAnArray => (Min)Heap

---
+++ JAVA/src/KthLargestElementInAnArray/Solution.java
@@ -0,0 +1,12 @@
+package KthLargestElementInAnArray;
+import java.util.*;
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for(int num : nums){
+            heap.offer(num);
+            if(heap.size() > k) heap.poll();
+        }
+        return heap.peek();
+    }
+}
