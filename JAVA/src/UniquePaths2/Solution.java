b4aad22240f69f00a987e99fc8559d6a909981cc
kimchanghee <kimchanghee94@naver.com>
2025-07-30 11:14:57 +0900

    #main 1. UniquePaths2 => 2d DP

---
+++ JAVA/src/UniquePaths2/Solution.java
@@ -0,0 +1,16 @@
+package UniquePaths2;
+
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m=obstacleGrid.length,n=obstacleGrid[0].length;
+        int[][] dp=new int[m][n];
+        if(obstacleGrid[0][0]==1) return 0;
+        dp[0][0]=1;
+        for(int i=1;i<m;i++) if(obstacleGrid[i][0]==0) dp[i][0]=dp[i-1][0];
+        for(int i=1;i<n;i++) if(obstacleGrid[0][i]==0) dp[0][i]=dp[0][i-1];
+        for(int i=1;i<m;i++) for(int j=1;j<n;j++)
+        if(obstacleGrid[i][j]==0) dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        else dp[i][j]=0;
+        return dp[m-1][n-1];
+    }
+}
