bb66cb327c1c5eaa3cdb9e1b3c11bafeb641ad8b
kimchanghee <kimchanghee94@naver.com>
2025-08-01 15:26:19 +0900

    #main 1, RemoveArray => Array

--- JAVA/src/RemoveArray/Solution.java
+++ JAVA/src/RemoveArray/Solution.java
@@ -13,3 +13,13 @@ public class Solution {
         return k;
     }
 }
+
+/*
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k=0;
+        for(int num : nums) if(num!=val) nums[k++]=num;
+        return k;
+    }
+}
+*/
