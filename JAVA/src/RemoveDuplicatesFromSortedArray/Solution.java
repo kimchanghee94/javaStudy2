5347f4f2a300face4156450cb8aab3297c8d6788
kimchanghee <kimchanghee94@naver.com>
2025-08-01 16:09:33 +0900

    #main 1. RemoveDuplicatesFromSortedArray => Array

--- JAVA/src/RemoveDuplicatesFromSortedArray/Solution.java
+++ JAVA/src/RemoveDuplicatesFromSortedArray/Solution.java
@@ -11,3 +11,13 @@ public class Solution {
         return k+1;
     }
 }
+
+/*
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k=1;
+        for(int i=1; i<nums.length;i++) if(nums[i-1]!=nums[i]) nums[k++]=nums[i];
+        return k;
+    }
+}
+*/
