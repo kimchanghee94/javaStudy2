b4bb2fa7cba9f104a78e3eb3677e48955cc6f3ff
kimchanghee <kimchanghee94@naver.com>
2025-08-02 12:18:22 +0900

    #main 1. BestTimeToBuyAndSellStock => Array

--- JAVA/src/BestTimeToBuyAndSellStock/Solution.java
+++ JAVA/src/BestTimeToBuyAndSellStock/Solution.java
@@ -17,3 +17,16 @@ public class Solution {
         return ans;
     }
 }
+
+/*
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min=Integer.MAX_VALUE, max=0;
+        for(int price : prices){
+            min=Math.min(min, price);
+            max=Math.max(max, price-min);
+        }
+        return max;
+    }
+}
+*/
