9605fec5cf7f2437ebcb89c7469a40aaf9be8110
kimchanghee <kimchanghee94@naver.com>
2025-08-02 12:36:11 +0900

    #main 1. JumpGame => Greedy

--- JAVA/src/JumpGame/Solution.java
+++ JAVA/src/JumpGame/Solution.java
@@ -15,3 +15,16 @@ public class Solution {
         return zero == 0 ? true : false;
     }
 }
+
+/*
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max=0;
+        for(int i=0; i<nums.length; i++){
+            if(max < i) return false;
+            max=Math.max(max, i+nums[i]);
+        }
+        return true;
+    }
+}
+*/
