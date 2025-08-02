5f621c5d9d82dc502155d90116f0870379d4aa37
kimchanghee <kimchanghee94@naver.com>
2025-08-02 13:17:00 +0900

    #main 1. JumpGame2 => Greedy

--- JAVA/src/JumpGame2/Solution.java
+++ JAVA/src/JumpGame2/Solution.java
@@ -18,3 +18,18 @@ public class Solution {
         return ans;
     }
 }
+/*
+class Solution {
+    public int jump(int[] nums) {
+        int j=0,e=0,f=0;
+        for(int i=0; i<nums.length-1; i++){
+            f=Math.max(f, i+nums[i]);
+            if(i==e){
+                j++;
+                e=f;
+            }
+        }
+        return j;
+    }
+}
+ */
