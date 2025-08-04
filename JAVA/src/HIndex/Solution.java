437b28e221ab038588100adbce11a0a06c9558dc
kimchanghee <kimchanghee94@naver.com>
2025-08-04 18:01:50 +0900

    #main 1. HIndex => Array

--- JAVA/src/HIndex/Solution.java
+++ JAVA/src/HIndex/Solution.java
@@ -19,3 +19,14 @@ public class Solution {
         return ans;
     }
 }
+
+/*
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n=citations.length;
+        for(int i=0; i<n; i++) if(n-i <= citations[i]) return n-i;
+        return 0;
+    }
+}
+*/
