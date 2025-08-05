a373601fc7c6d36d38c84ab3e35cdaab9309b781
kimchanghee <kimchanghee94@naver.com>
2025-08-05 12:37:06 +0900

    #main 1. GasStation => Greedy + Array

--- JAVA/src/GasStation/Solution.java
+++ JAVA/src/GasStation/Solution.java
@@ -19,3 +19,21 @@ public class Solution {
         return maxSum < 0 ? -1 : answer;
     }
 }
+
+/*
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total=0, tank=0, start=0;
+        for(int i=0; i<gas.length; i++){
+            int diff=gas[i]-cost[i];
+            total+=diff;
+            tank+=diff;
+            if(tank<0){
+                start=i+1;
+                tank=0;
+            }
+        }
+        return total>=0 ? start : -1;
+    }
+}
+*/
