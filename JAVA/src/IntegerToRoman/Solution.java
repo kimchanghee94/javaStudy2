b4dd4030031f6e6bc26bce0cfb64ab9dbc1b944c
kimchanghee <kimchanghee94@naver.com>
2025-08-06 11:49:42 +0900

    #main 1. IntegerToRoman => String + Array

--- JAVA/src/IntegerToRoman/Solution.java
+++ JAVA/src/IntegerToRoman/Solution.java
@@ -42,3 +42,23 @@ public class Solution {
         }
     }
 }
+
+
+/*
+class Solution {
+    public String intToRoman(int num) {
+        int[] values =    {1000, 900, 500, 400, 100, 90,  50,  40,  10,  9,   5,   4,   1};
+        String[] romans = {"M",  "CM","D", "CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<values.length; i++){
+            while(values[i]<=num){
+                num-=values[i];
+                sb.append(romans[i]);
+            }
+        }
+
+        return sb.toString();
+    }
+}
+*/
