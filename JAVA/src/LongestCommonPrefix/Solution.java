4734df35f4d7a194c3169cae7e52adc743c23a9a
kimchanghee <kimchanghee94@naver.com>
2025-08-06 12:18:07 +0900

    #main 1. LongestCommonPrefix => String + Array

--- JAVA/src/LongestCommonPrefix/Solution.java
+++ JAVA/src/LongestCommonPrefix/Solution.java
@@ -21,3 +21,18 @@ public class Solution {
         return prefix.toString();
     }
 }
+
+/*
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder ans = new StringBuilder(strs[0]);
+        for(int i=1; i<strs.length; i++){
+            while(!strs[i].startsWith(ans.toString())){
+                ans.deleteCharAt(ans.length()-1);
+                if(ans.length()==0) return "";
+            }
+        }
+        return ans.toString();
+    }
+}
+*/
