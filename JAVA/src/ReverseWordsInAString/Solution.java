ccf70018abc6e6ad3fdc010ec4fac0abd6a68041
kimchanghee <kimchanghee94@naver.com>
2025-08-06 12:23:44 +0900

    #main 1. ReverseWordsInAString => String

--- JAVA/src/ReverseWordsInAString/Solution.java
+++ JAVA/src/ReverseWordsInAString/Solution.java
@@ -13,3 +13,14 @@ public class Solution {
         return ans.toString().trim();
     }
 }
+
+/*
+class Solution {
+    public String reverseWords(String s) {
+        String[] arr = s.split("\\s+");
+        StringBuilder ans = new StringBuilder();
+        for(int i=arr.length-1; i>=0; i--) ans.append(arr[i] + " ");
+        return ans.toString().trim();
+    }
+}
+*/
