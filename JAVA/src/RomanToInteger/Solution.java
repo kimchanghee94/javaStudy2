6f855777bdba3dcc293a56df00a1993079769b7a
kimchanghee <kimchanghee94@naver.com>
2025-08-06 11:27:57 +0900

    #main 1. RomanToInteger => String + Array

--- JAVA/src/RomanToInteger/Solution.java
+++ JAVA/src/RomanToInteger/Solution.java
@@ -31,3 +31,31 @@ public class Solution {
         };
     }
 }
+
+/*
+class Solution {
+    public int romanToInt(String s) {
+        int ans=0;
+        for(int i=0; i<s.length(); i++){
+            int val=getVal(s.charAt(i));
+            int nextVal=i==s.length()-1 ? 0 : getVal(s.charAt(i+1));
+            if(val < nextVal) ans-=val;
+            else ans+=val;
+        }
+        return ans;
+    }
+
+    private int getVal(char c){
+        return switch(c){
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> 0;
+        };
+    }
+}
+*/
