17ee932f79e8e9c239c451447e6be8f9219b6582
kimchanghee <kimchanghee94@naver.com>
2025-08-07 15:06:05 +0900

    #main 1. ValidPalindrome => Two Pointer

--- JAVA/src/ValidPalindrome/Solution.java
+++ JAVA/src/ValidPalindrome/Solution.java
@@ -26,3 +26,22 @@ public class Solution {
         return ans;
     }
 }
+
+/*
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase();
+        int l=0,r=s.length()-1;
+        while(l<r){
+            if(!Character.isLetterOrDigit(s.charAt(l))) l++;
+            else if(!Character.isLetterOrDigit(s.charAt(r))) r--;
+            else if(s.charAt(l) != s.charAt(r)) return false;
+            else {
+                l++;
+                r--;
+            }
+        }
+        return true;
+    }
+}
+*/
