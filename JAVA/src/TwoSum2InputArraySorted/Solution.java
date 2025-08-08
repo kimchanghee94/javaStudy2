5a1c2e3f9c13644287c15a49647aeb90adf2b68f
kimchanghee <kimchanghee94@naver.com>
2025-08-08 10:06:50 +0900

    #main 1. TwoSum2InputArraySorted => two pointer

--- JAVA/src/TwoSum2InputArraySorted/Solution.java
+++ JAVA/src/TwoSum2InputArraySorted/Solution.java
@@ -19,3 +19,17 @@ public class Solution {
         return new int[]{left+1, right+1};
     }
 }
+
+/*
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int l=0, r=numbers.length-1;
+        while(l<r){
+            if(numbers[l]+numbers[r]==target) break;
+            else if(numbers[l]+numbers[r]<target) l++;
+            else r--;
+        }
+        return new int[]{l+1, r+1};
+    }
+}
+*/
