e31588a71219f256dec3d341718cf630c28296c5
kimchanghee <kimchanghee94@naver.com>
2025-08-08 11:22:19 +0900

    #main 1. MinimumSizeSubarraySum => Slinding Window

--- JAVA/src/MinimumSizeSubarraySum/Solution.java
+++ JAVA/src/MinimumSizeSubarraySum/Solution.java
@@ -18,3 +18,19 @@ public class Solution {
         return ans == Integer.MAX_VALUE ? 0 : ans;
     }
 }
+
+/*
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int l=0,sum=0,ans=Integer.MAX_VALUE;
+        for(int i=0; i<nums.length; i++){
+            sum+=nums[i];
+            while(sum>=target){
+                ans=Math.min(ans, i-l+1);
+                sum-=nums[l++];
+            }
+        }
+        return ans==Integer.MAX_VALUE?0:ans;
+    }
+}
+*/
