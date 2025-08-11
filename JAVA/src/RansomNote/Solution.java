4d1256c8d71a4c8a15c193c184c5f7f704d2f9ca
kimchanghee <kimchanghee94@naver.com>
2025-08-11 16:23:02 +0900

    #main 1. RansomNote => HashMap

--- JAVA/src/RansomNote/Solution.java
+++ JAVA/src/RansomNote/Solution.java
@@ -15,3 +15,14 @@ public class Solution {
         return true;
     }
 }
+
+/*
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] map = new int[26];
+        for(char c : magazine.toCharArray()) map[c-'a']++;
+        for(char c : ransomNote.toCharArray()) if((map[c-'a']--)==0) return false;
+        return true;
+    }
+}
+*/
