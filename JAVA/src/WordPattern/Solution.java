e9b168b13621f2a333d8e13662436a9245f3a72e
kimchanghee <kimchanghee94@naver.com>
2025-08-11 17:22:05 +0900

    #main 1. WordPattern => HashMap

--- JAVA/src/WordPattern/Solution.java
+++ JAVA/src/WordPattern/Solution.java
@@ -25,3 +25,22 @@ public class Solution {
         return true;
     }
 }
+
+/*
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words=s.split(" ");
+        if(pattern.length() != words.length) return false;
+        Map<Character, String> map = new HashMap<>();
+        for(int i=0; i<pattern.length(); i++){
+            char c=pattern.charAt(i);
+            String word=words[i];
+            if(!map.containsKey(c)){
+                if(map.containsValue(word)) return false;
+                map.put(c, word);
+            }else if(!map.get(c).equals(word)) return false;
+        }
+        return true;
+    }
+}
+*/
