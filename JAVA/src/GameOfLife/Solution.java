5be63ffdfeac0dd12e415442469f3791fd7878ea
kimchanghee <kimchanghee94@naver.com>
2025-08-11 16:19:16 +0900

    #main 1. GameOfLife => Matrix

--- JAVA/src/GameOfLife/Solution.java
+++ JAVA/src/GameOfLife/Solution.java
@@ -35,3 +35,28 @@ public class Solution {
         }
     }
 }
+
+/*
+class Solution {
+    public void gameOfLife(int[][] board) {
+        int m=board.length, n=board[0].length;
+        for(int i=0; i<m; i++) for(int j=0; j<n; j++){
+            int cnt=chk(i,j,m,n,board);
+            if(board[i][j]==0 && cnt==3) board[i][j]=-1;
+            else if(board[i][j]==1 && (cnt<2 || cnt>3)) board[i][j]=2;
+        }
+        for(int i=0; i<m; i++) for(int j=0; j<n; j++) if(board[i][j]==-1) board[i][j]=1; else if(board[i][j]==2) board[i][j]=0;
+    }
+
+    private int chk(int si, int sj, int m, int n, int[][] board){
+        int[][] dir={{0,-1},{0,1},{1,0},{-1,0},{-1,-1},{-1,1},{1,-1},{1,1}};
+        int cnt=0;
+        for(int d=0; d<dir.length; d++){
+            int ni=si+dir[d][0], nj=sj+dir[d][1];
+            if(ni<0||nj<0||ni>=m||nj>=n) continue;
+            if(board[ni][nj]==1 || board[ni][nj]==2) cnt++;
+        }
+        return cnt;
+    }
+}
+*/
