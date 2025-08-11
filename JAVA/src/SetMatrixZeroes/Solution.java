09849990ec23b65788b4893af33b952129e32cda
kimchanghee <kimchanghee94@naver.com>
2025-08-11 15:32:35 +0900

    #main 1. setZeroes => Matrix

--- JAVA/src/SetMatrixZeroes/Solution.java
+++ JAVA/src/SetMatrixZeroes/Solution.java
@@ -17,3 +17,21 @@ public class Solution {
         if(firC == true) for(int i=0; i<M; i++) matrix[i][0] = 0;
     }
 }
+
+/*
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean cflag=false,rflag=false;
+        int m=matrix.length, n=matrix[0].length;
+        for(int i=0; i<n && !rflag; i++) if(matrix[0][i]==0) rflag=true;
+        for(int i=0; i<m && !cflag; i++) if(matrix[i][0]==0) cflag=true;
+        for(int i=1; i<m; i++) for(int j=1; j<n; j++) if(matrix[i][j]==0) {
+            matrix[i][0]=0;
+            matrix[0][j]=0;
+        }
+        for(int i=1; i<m; i++) for(int j=1; j<n; j++) if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j]=0;
+        if(rflag) for(int i=0; i<n; i++) matrix[0][i]=0;
+        if(cflag) for(int i=0; i<m; i++) matrix[i][0]=0;
+    }
+}
+*/
