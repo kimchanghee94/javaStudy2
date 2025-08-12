6dee3099c1c02e9a6356655d8d0f19d62a576154
kimchanghee <kimchanghee94@naver.com>
2025-08-12 12:30:14 +0900

    #main 1. TwoSum => HashMap

--- JAVA/src/TwoSum/Solution.java
+++ JAVA/src/TwoSum/Solution.java
@@ -13,3 +13,17 @@ public class Solution {
         return new int[0];
     }
 }
+
+/*
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int val=target-nums[i];
+            if(map.containsKey(val)) return new int[]{i,map.get(val)};
+            else map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
+*/
