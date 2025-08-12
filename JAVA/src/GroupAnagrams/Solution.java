0009b988e7ef8cd0f1d7ca3b8ad5b95b8a4716dc
kimchanghee <kimchanghee94@naver.com>
2025-08-12 12:08:28 +0900

    #main 1. GroupAnagrams => HashMap

--- JAVA/src/GroupAnagrams/Solution.java
+++ JAVA/src/GroupAnagrams/Solution.java
@@ -14,3 +14,18 @@ public class Solution {
         return new ArrayList<>(map.values());
     }
 }
+
+/*
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String str : strs){
+            char[] c=str.toCharArray();
+            Arrays.sort(c);
+            String key=new String(c);
+            map.computeIfAbsent(key, k->new ArrayList<>()).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
+*/
