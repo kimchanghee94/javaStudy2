736ed0d4b71c8a8b0bac698422839cbdf162ba26
kimchanghee <kimchanghee94@naver.com>
2025-08-12 12:37:29 +0900

    #main 1. HappyNumber => Set

--- JAVA/src/HappyNumber/Solution.java
+++ JAVA/src/HappyNumber/Solution.java
@@ -18,3 +18,26 @@ public class Solution {
         return n==1;
     }
 }
+
+/*
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while(!set.contains(n)){
+            set.add(n);
+            n=chk(n);
+        }
+        return n==1;
+    }
+
+    private int chk(int n){
+        int total=0;
+        while(n>0){
+            int a=n%10;
+            total+=a*a;
+            n/=10;
+        }
+        return total;
+    }
+}
+*/
