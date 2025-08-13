29f57826ad0eb6733de406d06cfc91b3a351db4d
kimchanghee <kimchanghee94@naver.com>
2025-08-13 11:34:53 +0900

    #main 1. LongestConsecutiveSequence => Set

--- JAVA/src/LongestConsecutiveSequence/Solution.java
+++ JAVA/src/LongestConsecutiveSequence/Solution.java
@@ -20,3 +20,21 @@ public class Solution {
         return ans;
     }
 }
+
+/*
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int ans=0;
+        for(int num:nums) set.add(num);
+        for(int num:set){
+            if(!set.contains(num-1)){
+                int cnt=0;
+                while(set.contains(num++)) cnt++;
+                ans=Math.max(ans, cnt);
+            }
+        }
+        return ans;
+    }
+}
+*/
