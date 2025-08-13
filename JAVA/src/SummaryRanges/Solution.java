61ee9be0ee15da291d1990d1cb45e1ba12ee02ae
kimchanghee <kimchanghee94@naver.com>
2025-08-13 11:48:15 +0900

    #main 1. SummaryRanges => Interval

--- JAVA/src/SummaryRanges/Solution.java
+++ JAVA/src/SummaryRanges/Solution.java
@@ -19,3 +19,20 @@ public class Solution {
         return ans;
     }
 }
+
+/*
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> ans = new ArrayList<>();
+        int strt = 0;
+        for(int i=1; i<=nums.length; i++){
+            if(i==nums.length || nums[i-1]+1!=nums[i]){
+                if(nums[strt]==nums[i-1]) ans.add(String.valueOf(nums[strt]));
+                else ans.add(nums[strt]+"->"+nums[i-1]);
+                strt=i;
+            }
+        }
+        return ans;
+    }
+}
+*/
