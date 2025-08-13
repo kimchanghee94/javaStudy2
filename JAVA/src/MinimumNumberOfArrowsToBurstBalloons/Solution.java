9502ad12c5a6dbda8b9abbfb7943554c174fac93
kimchanghee <kimchanghee94@naver.com>
2025-08-13 12:29:02 +0900

    #main 1. MinimumNumberOfArrowsToBurstBalloons => Intervals

--- JAVA/src/MinimumNumberOfArrowsToBurstBalloons/Solution.java
+++ JAVA/src/MinimumNumberOfArrowsToBurstBalloons/Solution.java
@@ -15,3 +15,19 @@ public class Solution {
         return ans;
     }
 }
+
+/*
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, (a,b)->Integer.compare(a[1],b[1]));
+        int ans=1,e=points[0][1];
+        for(int[] point:points){
+            if(e<point[0]){
+                ans++;
+                e=point[1];
+            }
+        }
+        return ans;
+    }
+}
+*/
