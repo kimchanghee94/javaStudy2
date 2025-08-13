a002097b8032ae3818dd870e14cda371980b50d3
kimchanghee <kimchanghee94@naver.com>
2025-08-13 12:18:19 +0900

    #main 1. InsertInterval => Interval

--- JAVA/src/InsertInterval/Solution.java
+++ JAVA/src/InsertInterval/Solution.java
@@ -17,3 +17,18 @@ public class Solution {
         return ans.toArray(new int[ans.size()][]);
     }
 }
+
+/*
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> ans = new ArrayList<>();
+        int i=0, N=intervals.length;
+        while(i<N && intervals[i][1] < newInterval[0]) ans.add(intervals[i++]);
+        while(i<N && intervals[i][0] <= newInterval[1])
+        newInterval=new int[]{Math.min(intervals[i][0], newInterval[0]), Math.max(intervals[i++][1], newInterval[1])};
+        ans.add(newInterval);
+        while(i<N) ans.add(intervals[i++]);
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
+*/
