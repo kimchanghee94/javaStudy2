17d83b3961d593ed5fdbe47d78e859b20ad4ac35
kimchanghee <kimchanghee94@naver.com>
2025-08-14 11:07:21 +0900

    #main 1. SimplifyPath => Stack

--- JAVA/src/SimplifyPath/Solution.java
+++ JAVA/src/SimplifyPath/Solution.java
@@ -16,3 +16,17 @@ public class Solution {
         return "/"+String.join("/",s);
     }
 }
+
+/*
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> s = new Stack<>();
+        String[] dirs = path.split("/");
+        for(String dir : dirs){
+            if(dir.equals("..") && !s.isEmpty()) s.pop();
+            else if(!dir.equals(".") && !dir.isEmpty() && !dir.equals("..")) s.push(dir);
+        }
+        return "/"+String.join("/", s);
+    }
+}
+*/
