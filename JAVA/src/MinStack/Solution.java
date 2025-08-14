6be62f45d5a8c98328183979200055aef371a923
kimchanghee <kimchanghee94@naver.com>
2025-08-14 11:27:54 +0900

    #main 1. MinStack => Stack

--- JAVA/src/MinStack/Solution.java
+++ JAVA/src/MinStack/Solution.java
@@ -29,3 +29,35 @@ public class Solution {
         return ms[mt];
     }
 }
+
+/*
+class MinStack {
+    int[] arr, mArr;
+    int top, mTop;
+
+    public MinStack() {
+        arr=new int[30005];
+        mArr=new int[30005];
+        top=0;
+        mTop=0;
+    }
+
+    public void push(int val) {
+        if(mTop==0 || val<=mArr[mTop-1]) mArr[mTop++]=val;
+        arr[top++]=val;
+    }
+
+    public void pop() {
+        if(mArr[mTop-1]==arr[top-1]) mTop--;
+        top--;
+    }
+
+    public int top() {
+        return arr[top-1];
+    }
+
+    public int getMin() {
+        return mArr[mTop-1];
+    }
+}
+*/
