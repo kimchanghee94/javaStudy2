e25c28c6976e1f312c2aa72866ebc9748df40ef7
kimchanghee <kimchanghee94@naver.com>
2025-08-14 10:53:47 +0900

    #main 1. ValidParentheses => stack

--- JAVA/src/ValidParentheses/Solution.java
+++ JAVA/src/ValidParentheses/Solution.java
@@ -18,3 +18,19 @@ public class Solution {
         return stack.isEmpty();
     }
 }
+
+/*
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c=='(' || c=='{' || c== '[') stack.push(c);
+            else if(stack.isEmpty()) return false;
+            else if(c==')' && stack.pop()!='(') return false;
+            else if(c=='}' && stack.pop()!='{') return false;
+            else if(c==']' && stack.pop()!='[') return false;
+        }
+        return stack.isEmpty();
+    }
+}
+*/
