56795d72ffb6b72bd13b4b151a4c071cbe6a6130
kimchanghee <kimchanghee94@naver.com>
2025-08-21 10:44:49 +0900

    #main 1. BasicCalculator => Stack

--- JAVA/src/BasicCalculator/Solution.java
+++ JAVA/src/BasicCalculator/Solution.java
@@ -34,3 +34,42 @@ public class Solution {
         return ans + (sign*num);
     }
 }
+
+/*
+class Solution {
+    public int calculate(String s) {
+        int ans=0,num=0,sign=1;
+        Stack<Integer> stack=new Stack<>();
+        for(char c : s.toCharArray()){
+            switch(c){
+                case '+'->{
+                    ans+=sign*num;
+                    num=0;
+                    sign=1;
+                }
+                case '-'->{
+                    ans+=sign*num;
+                    num=0;
+                    sign=-1;
+                }
+                case '('->{
+                    stack.push(ans);
+                    stack.push(sign);
+                    ans=0;
+                    sign=1;
+                }
+                case ')'->{
+                    ans+=sign*num;
+                    num=0;
+                    ans*=stack.pop();
+                    ans+=stack.pop();
+                }
+                default->{
+                    if(c!=' ') num=num*10+(c-'0');
+                }
+            }
+        }
+        return ans+(sign*num);
+    }
+}
+*/
