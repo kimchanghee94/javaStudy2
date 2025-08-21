1f1d0fc7dfaf1f5c209e3b03e35417a76583e08f
kimchanghee <kimchanghee94@naver.com>
2025-08-21 11:05:18 +0900

    #main 1. AddTwoNumbers => LinkedList

--- JAVA/src/AddTwoNumbers/Solution.java
+++ JAVA/src/AddTwoNumbers/Solution.java
@@ -33,3 +33,28 @@ public class Solution {
         return dummy.next;
     }
 }
+
+/*
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        int up=0;
+        while(l1!=null || l2!=null || up>0){
+            int sum=up;
+            if(l1!=null){
+                sum+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null){
+                sum+=l2.val;
+                l2=l2.next;
+            }
+            up=sum/10;
+            cur.next=new ListNode(sum%10);
+            cur=cur.next;
+        }
+        return dummy.next;
+    }
+}
+*/
