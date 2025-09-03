2c2a19236a9df61b44f5faa172f6e75472aba63a
kimchanghee <kimchanghee94@naver.com>
2025-09-03 12:14:14 +0900

    #main 1. hasPathSum => BinaryTree

--- JAVA/src/PathSum/Solution.java
+++ JAVA/src/PathSum/Solution.java
@@ -28,3 +28,17 @@ public class Solution {
         return dfs(root.left, targetSum, sum) || dfs(root.right, targetSum, sum);
     }
 }
+
+/*
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return dfs(root, 0, targetSum);
+    }
+
+    private boolean dfs(TreeNode root, int cur, int target){
+        if(root == null) return false;
+        if(cur+root.val == target && root.left==null && root.right==null) return true;
+        return dfs(root.left, cur+root.val, target) || dfs(root.right,cur+root.val, target);
+    }
+}
+*/
