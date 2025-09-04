91e71cc6ddc26098ce05c842fdda4b6fa182bef1
kimchanghee <kimchanghee94@naver.com>
2025-09-04 12:51:56 +0900

    #main 1. LowestCommonAncestorOfABinaryTree => BinaryTree

--- JAVA/src/LowestCommonAncestorOfABinaryTree/Solution.java
+++ JAVA/src/LowestCommonAncestorOfABinaryTree/Solution.java
@@ -16,3 +16,15 @@ public class Solution {
         return left != null ? left : right;
     }
 }
+
+/*
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null||root==p||root==q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left!=null && right != null) return root;
+        return left==null ? right : left;
+    }
+}
+*/
