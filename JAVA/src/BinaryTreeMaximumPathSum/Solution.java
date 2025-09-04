c53fd9bf84c81ef5529185eb9fbf7e7d75d7648c
kimchanghee <kimchanghee94@naver.com>
2025-09-04 11:16:39 +0900

    #main 1. BinaryTreeMaximumPathSum => BinaryTree

--- JAVA/src/BinaryTreeMaximumPathSum/Solution.java
+++ JAVA/src/BinaryTreeMaximumPathSum/Solution.java
@@ -31,3 +31,22 @@ class Solution {
         return root.val + Math.max(leftMax, rightMax);
     }
 }
+
+/*
+class Solution {
+    int ans=Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    private int dfs(TreeNode root){
+        if(root==null) return 0;
+        int lM=Math.max(dfs(root.left), 0);
+        int rM=Math.max(dfs(root.right), 0);
+        int cM=lM+rM+root.val;
+        ans=Math.max(cM, ans);
+        return Math.max(lM,rM)+root.val;
+    }
+}
+*/
