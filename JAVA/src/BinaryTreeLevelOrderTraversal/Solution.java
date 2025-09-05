cfdf066e4098df8911573c9293152c0e2574c0de
kimchanghee <kimchanghee94@naver.com>
2025-09-05 10:19:04 +0900

    #main 1. BinaryTreeLevelOrderTraversal => BinaryTree + BFS

--- JAVA/src/BinaryTreeLevelOrderTraversal/Solution.java
+++ JAVA/src/BinaryTreeLevelOrderTraversal/Solution.java
@@ -36,3 +36,27 @@ public class Solution {
         return ans;
     }
 }
+
+/*
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root==null) return ans;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> arr = new ArrayList<>();
+            for(int i=0; i<size; i++){
+                TreeNode tmp = q.poll();
+                arr.add(tmp.val);
+                if(tmp.left!=null) q.offer(tmp.left);
+                if(tmp.right!=null) q.offer(tmp.right);
+            }
+            ans.add(arr);
+        }
+        return ans;
+    }
+}
+*/
