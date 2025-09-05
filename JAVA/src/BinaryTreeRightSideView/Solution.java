08ee8f4f07551425617293c0ce8e1a6025fcaa08
kimchanghee <kimchanghee94@naver.com>
2025-09-05 10:03:07 +0900

    #main 1. BinaryTreeRightSideView => BinaryTree + BFS

--- JAVA/src/BinaryTreeRightSideView/Solution.java
+++ JAVA/src/BinaryTreeRightSideView/Solution.java
@@ -39,3 +39,24 @@ public class Solution {
         return ans;
     }
 }
+
+/*
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if(root==null) return ans;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                TreeNode tmp = q.poll();
+                if(i==size-1) ans.add(tmp.val);
+                if(tmp.left!=null) q.offer(tmp.left);
+                if(tmp.right!=null) q.offer(tmp.right);
+            }
+        }
+        return ans;
+    }
+}
+*/
