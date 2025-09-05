72c5664e1fc8cd8aefd48f51598845f239a9ddb6
kimchanghee <kimchanghee94@naver.com>
2025-09-05 10:39:43 +0900

    #main 1. BinaryTreeZigZagLevelOrderTraversal => BinaryTree + BFS

--- JAVA/src/BinaryTreeZigZagLevelOrderTraversal/Solution.java
+++ JAVA/src/BinaryTreeZigZagLevelOrderTraversal/Solution.java
@@ -43,3 +43,32 @@ public class Solution {
         return ans;
     }
 }
+
+/*
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root==null) return ans;
+        boolean dir = false;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> arr = new ArrayList<>();
+
+            for(int i=0; i<size; i++){
+                TreeNode tmp = q.poll();
+                arr.add(tmp.val);
+                if(tmp.left!=null) q.offer(tmp.left);
+                if(tmp.right!=null) q.offer(tmp.right);
+            }
+            if(dir) Collections.reverse(arr);
+            ans.add(arr);
+            dir=!dir;
+        }
+
+        return ans;
+    }
+}
+*/
