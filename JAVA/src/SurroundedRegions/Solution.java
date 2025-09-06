b811df19a3ea9c9c7c09e8d8daf556cda335540c
kimchanghee <kimchanghee94@naver.com>
2025-09-06 11:01:42 +0900

    #main 1. SurroundedRegions => Graph

--- JAVA/src/SurroundedRegions/Solution.java
+++ JAVA/src/SurroundedRegions/Solution.java
@@ -34,3 +34,30 @@ public class Solution {
         }
     }
 }
+
+/*
+class Solution {
+    int[] di=new int[]{1,0,-1,0};
+    int[] dj=new int[]{0,1,0,-1};
+    int M,N;
+    public void solve(char[][] board) {
+        M=board.length; N=board[0].length;
+        for(int i=0; i<M; i++) if(board[i][0]=='O') dfs(board,i,0);
+        for(int i=0; i<M; i++) if(board[i][N-1]=='O') dfs(board,i,N-1);
+        for(int i=0; i<N; i++) if(board[0][i]=='O') dfs(board,0,i);
+        for(int i=0; i<N; i++) if(board[M-1][i]=='O') dfs(board,M-1,i);
+        for(int i=0; i<M; i++) for(int j=0; j<N; j++){
+            if(board[i][j]=='1') board[i][j]='O';
+            else board[i][j]='X';
+        }
+    }
+    private void dfs(char[][] board, int si, int sj){
+        board[si][sj]='1';
+        for(int d=0; d<4; d++){
+            int ni=si+di[d], nj=sj+dj[d];
+            if(ni<0||nj<0||ni>=M||nj>=N||board[ni][nj]!='O') continue;
+            dfs(board, ni, nj);
+        }
+    }
+}
+*/
