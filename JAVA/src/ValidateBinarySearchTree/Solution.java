253bb25117a37aed944917402ea4b88c7224c7fd
kimchanghee <kimchanghee94@naver.com>
2025-09-06 10:31:06 +0900

    #main 1. ValidateBinarySearchTree => BST

--- JAVA/src/ValidateBinarySearchTree/Solution.java
+++ JAVA/src/ValidateBinarySearchTree/Solution.java
@@ -24,3 +24,17 @@ public class Solution {
         return left&&right;
     }
 }
+
+/*
+class Solution {
+    TreeNode prev = null;
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) return true;
+        boolean left=isValidBST(root.left);
+        if(prev!=null && prev.val >= root.val) return false;
+        prev=root;
+        boolean right=isValidBST(root.right);
+        return left&&right;
+    }
+}
+*/
