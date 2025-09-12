c0187592c0479e06b7a30caac828b6d8880bb684
kimchanghee <kimchanghee94@naver.com>
2025-09-12 10:34:45 +0900

    #main 1. CourseSchedule => Graph

--- JAVA/src/CourseSchedule/Solution.java
+++ JAVA/src/CourseSchedule/Solution.java
@@ -30,3 +30,24 @@ public class Solution {
         return false;
     }
 }
+
+/*
+class Solution {
+    int[] flag;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for(int[] pre : prerequisites) graph.computeIfAbsent(pre[1], k->new ArrayList<>()).add(pre[0]);
+        flag=new int[numCourses];
+        for(int i=0; i<numCourses; i++) if(cycle(graph, i)) return false;
+        return true;
+    }
+    private boolean cycle(Map<Integer, List<Integer>> graph, int strt){
+        if(flag[strt]==1) return true;
+        if(flag[strt]==2) return false;
+        flag[strt]=1;
+        if(graph.containsKey(strt)) for(int next:graph.get(strt)) if(cycle(graph, next)) return true;
+        flag[strt]=2;
+        return false;
+    }
+}
+*/
