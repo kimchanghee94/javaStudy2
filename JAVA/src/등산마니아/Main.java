fa8882b43f9efac6396b85fde61d768990a42037
kimchanghee <kimchanghee94@naver.com>
2026-01-25 11:55:40 +0900

    #main 1. 등산 마니아 => DFS + DP, 간선의 개수 수학식

--- JAVA/src/등산마니아/Main.java
+++ JAVA/src/등산마니아/Main.java
@@ -3,19 +3,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    private static int N;
+    private static long ans;
+    private static List<Integer>[] graph;
+    private static long[] dp;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        graph = new ArrayList[N+1];
+        dp = new long[N+1];
+        for(int i=1;i<=N;i++) graph[i]=new ArrayList<>();
 
-        for(int i=0;i<N;i++){
+        for(int i=0;i<N-1;i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
+            graph[s].add(e);
+            graph[e].add(s);
         }
-
-
+        dfs(1);
+        bw.write(String.valueOf(ans-((long)N*(N-1)/2)));
         br.close();
         bw.close();
+    }
+
+    private static long dfs(int s){
+        dp[s]=1;
+        for(int e : graph[s]) if(dp[e]==0) dp[s]+=dfs(e);
+        ans+=(((long)N*(N-1))/2) - (((N-dp[s])*(N-dp[s]-1))/2);
+        return dp[s];
     }
 }
