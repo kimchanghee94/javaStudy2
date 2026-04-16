c254e8c783e944b5c40ad4011922508fabdf6aeb
kimchanghee <kimchanghee94@naver.com>
2026-04-16 15:38:40 +0900

    #main 1. 숫자고르기 => DFS

---
+++ JAVA/src/숫자고르기_2/Main.java
@@ -0,0 +1,34 @@
+package 숫자고르기_2;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static List<Integer> ans = new ArrayList<>();
+    private static int[] arr;
+    private static boolean[] flag;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N+1];
+        flag = new boolean[N+1];
+        for(int i=1;i<=N;i++) arr[i] = Integer.parseInt(br.readLine());
+        for(int i=1;i<=N;i++) dfs(i,i);
+
+        bw.write(ans.size()+"\n");
+        for(int tmp : ans) bw.write(tmp+"\n");
+        br.close();
+        bw.close();
+    }
+
+    private static void dfs(int s, int e){
+        if(flag[e]) {
+            if (s == e) ans.add(s);
+        }
+        else{
+            flag[e]=true;
+            dfs(s,arr[e]);
+            flag[e]=false;
+        }
+    }
+}
